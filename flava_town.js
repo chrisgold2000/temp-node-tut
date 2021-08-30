5311a457ec4824291177ab6a0c4cbf2372f142a5
Chris <chrisgold2000@hotmail.com>
2021-08-30 22:32:48 +0100

    first commit

---
+++ flava_town.js
@@ -0,0 +1,8 @@
+module.exports.items = ['item1','item2']
+const roberCat = {
+    name: 'rober',
+    age:2,
+    aminal: 'lovely cat'
+}
+
+module.exports.singlePerson = roberCat
---
+++ names.js
@@ -0,0 +1,7 @@
+const secret = 'SUPER SECRET'
+const peter = 'peter'
+const rover = 'rover'
+
+module.exports = {peter, rover}
+
+//console.log(module)
